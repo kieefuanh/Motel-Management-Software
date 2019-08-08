6819ac657dd193fe1d40059d25f3b6b82ce63372
Jason Heath <ntns1607@gmail.com>
2019-08-08 15:40:52 +0700

    .

--- CNPM/frmDichVu.cs
+++ CNPM/frmDichVu.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using BAL;
+using DTO;
 namespace CNPM
 {
     public partial class frmDichVu : Form
